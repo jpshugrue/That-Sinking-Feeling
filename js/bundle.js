cd4f553b0b92158e05fd28bafae7dcf8ccbd61b7
Jeremiah Shugrue <jpshugrue@gmail.com>
2018-01-18 10:21:58 -0500

    Refactor check collision method

--- js/bundle.js
+++ js/bundle.js
@@ -145,28 +145,28 @@ class Board {
     let then = this.now;
     this.now = Date.now();
     let timeDiff = (this.now - then) / 1000.0;
-    this.update(timeDiff);
+    this.update(timeDiff, this.player, this.map);
     this.render();
     window.requestAnimationFrame(this.main);
   }
 
-  update(timeDiff) {
+  update(timeDiff, player, map) {
     if (this.player.left) {
       this.player.xVel = (this.player.xVel - (this.MAX_HORIZONTAL_VEL * timeDiff));
-      this.checkForCollisions();
+      this.checkForCollisions(player, map);
       if (Math.abs(this.player.xVel) > this.MAX_HORIZONTAL_VEL) { this.player.xVel = -(this.MAX_HORIZONTAL_VEL); }
       this.player.x += this.player.xVel;
     }
     if (this.player.right) {
       this.player.xVel = (this.player.xVel + (this.MAX_HORIZONTAL_VEL * timeDiff));
-      this.checkForCollisions();
+      this.checkForCollisions(player, map);
       if (this.player.xVel > this.MAX_HORIZONTAL_VEL) { this.player.xVel = this.MAX_HORIZONTAL_VEL; }
       this.player.x += this.player.xVel;
     }
     if (this.player.jump) {
      if (this.player.yVel === 0) { this.player.yVel = this.MAX_VERTICAL_VEL; }
     }
-    this.checkForCollisions();
+    this.checkForCollisions(player, map);
     this.player.y += this.player.yVel;
 
     this.handleFriction(timeDiff);
@@ -179,55 +179,38 @@ class Board {
     return [row, column];
   }
 
-  checkForCollisions() {
-    const nextX = this.player.x + this.player.xVel;
-    const nextY = this.player.y + this.player.yVel;
-    let tilePos = this.getTilePos(nextX, nextY);
+  checkForCollisions(player, map) {
+    const nextX = player.x + player.xVel;
+    const nextY = player.y + player.yVel;
+    const tilePos = this.getTilePos(nextX, nextY);
+    const nextRow = tilePos[0];
+    const nextCol = tilePos[1];
 
-
-    if (this.player.xVel < 0) {
-      if (this.player.y % this.TILE_SIZE < 1) {
-        if (this.map[tilePos[0]][tilePos[1]].collides) {
-          this.player.xVel = 0;
-          this.player.x = this.map[tilePos[0]][tilePos[1]].x + this.TILE_SIZE;
-        }
-      } else if (this.map[tilePos[0]][tilePos[1]].collides || this.map[tilePos[0]][tilePos[1]+1].collides) {
-        this.player.xVel = 0;
-        this.player.x = this.map[tilePos[0]][tilePos[1]].x + this.TILE_SIZE;
+    if (player.xVel < 0) {
+      if ((player.y % this.TILE_SIZE < 1 && map[nextRow][nextCol].collides) ||
+        (player.y % this.TILE_SIZE >= 1 && (map[nextRow][nextCol].collides || map[nextRow+1][nextCol].collides))) {
+          player.xVel = 0;
+          player.x = map[nextRow][nextCol].x + this.TILE_SIZE;
       }
-    } else if (this.player.xVel > 0) {
-      if (this.player.y % this.TILE_SIZE < 1) {
-        if (this.map[tilePos[0]][tilePos[1]+1].collides) {
-          this.player.xVel = 0;
-          this.player.x = this.map[tilePos[0]][tilePos[1]+1].x - this.TILE_SIZE;
-        }
-      } else if (this.map[tilePos[0]][tilePos[1]+1].collides || this.map[tilePos[0]+1][tilePos[1]+1].collides) {
-        this.player.xVel = 0;
-        this.player.x = this.map[tilePos[0]][tilePos[1]+1].x - this.TILE_SIZE;
+    } else if (player.xVel > 0) {
+      if ((player.y % this.TILE_SIZE < 1 && map[nextRow][nextCol+1].collides) ||
+        (player.y % this.TILE_SIZE >= 1 && (map[nextRow][nextCol+1].collides || map[nextRow+1][nextCol+1].collides))) {
+          player.xVel = 0;
+          player.x = map[nextRow][nextCol+1].x - this.TILE_SIZE;
       }
     }
-    if (this.player.yVel < 0) {
-        if (this.player.x % this.TILE_SIZE < 1) {
-          if (this.map[tilePos[0]][tilePos[1]].collides) {
-            this.player.yVel = 0;
-            this.player.y = this.map[tilePos[0]][tilePos[1]].y + this.TILE_SIZE;
-          }
-        } else if (this.map[tilePos[0]][tilePos[1]].collides || this.map[tilePos[0]][tilePos[1]+1].collides) {
-          this.player.yVel = 0;
-          this.player.y = this.map[tilePos[0]][tilePos[1]].y + this.TILE_SIZE;
-        }
-    } else if (this.player.yVel > 0) {
-      if (this.player.x % this.TILE_SIZE < 1) {
-        if (this.map[tilePos[0]+1][tilePos[1]].collides) {
-          this.player.yVel = 0;
-          this.player.y = this.map[tilePos[0]+1][tilePos[1]].y - this.TILE_SIZE;
-        }
-      } else if (this.map[tilePos[0]+1][tilePos[1]].collides || this.map[tilePos[0]+1][tilePos[1]+1].collides) {
-        this.player.yVel = 0;
-        this.player.y = this.map[tilePos[0]+1][tilePos[1]].y - this.TILE_SIZE;
+    if (player.yVel < 0) {
+      if ((player.x % this.TILE_SIZE < 1 && map[nextRow][nextCol].collides) ||
+        (player.x % this.TILE_SIZE >= 1 && (map[nextRow][nextCol].collides || map[nextRow][nextCol+1].collides))) {
+          player.yVel = 0;
+          player.y = map[nextRow][nextCol].y + this.TILE_SIZE;
       }
-
-
+    } else if (player.yVel > 0) {
+      if ((player.x % this.TILE_SIZE < 1 && map[nextRow+1][nextCol].collides) ||
+        (player.x % this.TILE_SIZE >= 1 && (map[nextRow+1][nextCol].collides || map[nextRow+1][nextCol+1].collides))) {
+          player.yVel = 0;
+          player.y = map[nextRow+1][nextCol].y - this.TILE_SIZE;
+      }
     }
   }
 
@@ -294,6 +277,9 @@ class Board {
     map[22][9] = new __WEBPACK_IMPORTED_MODULE_1__tile__["a" /* default */]([90,220], true, 10, "green");
     map[22][10] = new __WEBPACK_IMPORTED_MODULE_1__tile__["a" /* default */]([100,220], true, 10, "green");
     map[22][11] = new __WEBPACK_IMPORTED_MODULE_1__tile__["a" /* default */]([110,220], true, 10, "green");
+    map[21][6] = new __WEBPACK_IMPORTED_MODULE_1__tile__["a" /* default */]([60,210], true, 10, "green");
+    map[21][7] = new __WEBPACK_IMPORTED_MODULE_1__tile__["a" /* default */]([70,210], true, 10, "green");
+    map[21][8] = new __WEBPACK_IMPORTED_MODULE_1__tile__["a" /* default */]([80,210], true, 10, "green");
     map[20][16] = new __WEBPACK_IMPORTED_MODULE_1__tile__["a" /* default */]([160,200], true, 10, "green");
     map[20][17] = new __WEBPACK_IMPORTED_MODULE_1__tile__["a" /* default */]([170,200], true, 10, "green");
     map[20][18] = new __WEBPACK_IMPORTED_MODULE_1__tile__["a" /* default */]([180,200], true, 10, "green");
